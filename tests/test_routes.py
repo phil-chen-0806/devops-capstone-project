00c65f215e85e8df09d904067d2e7d37db6c9b60
phil-chen-0806 <cp18918509809@163.com>
2024-08-03 05:36:21 -0400

    complete others

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -198,6 +198,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_method_not_allowed(self):
-        """It should not allow an illegal method call(by cp)"""
+        """It should not allow an illegal method call(by cp  )"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
