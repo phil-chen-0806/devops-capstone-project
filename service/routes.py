2dd61dc8920450e589e66c9b49d9f1c3408e84dd
phil-chen-0806 <cp18918509809@163.com>
2024-08-04 03:49:12 -0400

    “fix-lint”

--- service/routes.py
+++ service/routes.py
@@ -63,6 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -94,7 +95,7 @@ def read_account(id):
     if not account:
         abort(
             status.HTTP_404_NOT_FOUND,
-            #f"Account with id [{account_id}] could not be found.",
+            # f"Account with id [{account_id}] could not be found.",
         )
     message = account.serialize()
     return make_response(
@@ -106,6 +107,7 @@ def read_account(id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -127,6 +129,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
